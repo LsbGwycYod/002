2c2f3a93b3ae38d375ca77f1f370024593534cab
LsbGwycYod <2227397951@qq.com>
2024-10-14 17:07:26 +0800

    Add files via upload

---
+++ Java Web/0923maven-hutool/src/main/java/cn/nbpt/lilingyi/mavenhutool/MavenHutoolApplication.java
@@ -0,0 +1,13 @@
+package cn.nbpt.lilingyi.mavenhutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenHutoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavenHutoolApplication.class, args);
+    }
+
+}

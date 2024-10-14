1352a39c7f34f65691f845542af4371a20c5ff51
LsbGwycYod <2227397951@qq.com>
2024-10-14 16:56:59 +0800

    Add files via upload

---
+++ Java Web/0923maven-hutool/src/test/java/cn/nbpt/lilingyi/mavenhutool/MavenHutoolApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.nbpt.lilingyi.mavenhutool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenHutoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Java Web/1009hello-web/src/main/java/cn/edu/nbpt/lilingyi/helloweb/Address.java
@@ -0,0 +1,11 @@
+package cn.edu.nbpt.lilingyi.helloweb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Address {
+    private String province;
+    private String city;
+}
